51ac4288b9f4c6bb5dc67c4bb4b0c98d23a0af56
ducviet98 <viet.chu@digitalfortress.dev>
2023-03-03 16:04:29 +0700

    fix: fix pagination

--- src/containers/Devices/store/services/index.ts
+++ src/containers/Devices/store/services/index.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 import { ParamsType } from 'src/containers/Devices/interface';
 import AxiosClientInstance from 'src/utils/axios';
+import { formatPage } from 'src/utils/formatPage';
 
-export const getUserDeviceService = async (data: ParamsType) =>
-  await AxiosClientInstance.get(
-    `/api/devices?search=${data.search}&limit=${data.rowsPerPage}&offset=${data.page}`
-  );
+export const getUserDeviceService = async (data: ParamsType) => await AxiosClientInstance.get(
+  `/api/devices?search=${data.search}&limit=${data.rowsPerPage}&offset=${formatPage(data.rowsPerPage, data.page)}`
+)
 
 export const getBrandsService = async () =>
   await AxiosClientInstance.get(`/api/brands`);
